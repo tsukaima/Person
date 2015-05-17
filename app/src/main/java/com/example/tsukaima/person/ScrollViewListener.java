029d55873b235b519b921e5b6b882a81eb1320e8
tsukaima <1320396800@qq.com>
2015-05-17 23:12:13 +0800

    Revert "创建"
    This reverts commit e61f1db34a841beb248671241006fbcc6aca245e.

--- app/src/main/java/com/example/tsukaima/person/ScrollViewListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.tsukaima.person;
-
-/**
- * Created by chauvet on 15-5-17.
- */
-public interface ScrollViewListener {
-    void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
-}
